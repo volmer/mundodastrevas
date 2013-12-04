aa1f5d560a9b26b316b678114313e55a76d168f4
Volmer Soares <volmerius@gmail.com>
2013-12-03 22:12:07 -0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mundodastrevas::Application.config.secret_key_base = 'f079a5e6c17083d6aa531f4925a14cdf696b57c6310539fd578b61a199e0c4be31c4c214b43fdb5120be2ef01101fc5bdfe238805240baf44dc439640ebe062c'
