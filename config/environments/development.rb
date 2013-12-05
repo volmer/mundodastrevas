f49287a80c30263d37bb89074a30c5b666c934bd
Volmer Soares <volmerius@gmail.com>
2013-12-05 11:16:41 -0200

    Settings for Action Mailer.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ Mundodastrevas::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Mailcatcher
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
