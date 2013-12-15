fde05a198144f9d490f53c8ef6865b2151999ccc
Volmer Soares <volmerius@gmail.com>
2013-12-14 23:05:02 -0200

    Scenario for the home page.

--- features/support/shared_helpers.rb
+++ features/support/shared_helpers.rb
@@ -1,6 +1,6 @@
 module SharedHelpers
   def path_to(page_name)
-    send(page_name.split(/\s+/).push('path').join('_').to_sym)
+    raddar.send(page_name.split(/\s+/).push('path').join('_').to_sym)
   end
 end
 
