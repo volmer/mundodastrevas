5d905d727cf314f8acd8e4339159e9b0872b9cd8
Volmer Soares <volmerius@gmail.com>
2013-12-18 01:08:12 -0200

    Sendgrid configs.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,18 @@ Mundodastrevas::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.smtp_settings = {
+    address:         'smtp.sendgrid.net',
+    port:            '587',
+    authentication:  :plain,
+    user_name:       'mundodastrevas',
+    password:        'mentecapto12',
+    domain:          'mundodastrevas.com'
+  }
+
+  config.action_mailer.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
