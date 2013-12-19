f0be1b374dd22e8be65ab4b05ed638b441ce2b2e
Volmer Soares <volmerius@gmail.com>
2013-12-19 11:57:11 -0200

    Compress assets.

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module Mundodastrevas
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = 'pt-BR'
+
+    config.assets.initialize_on_precompile = false
+    config.assets.compress = true
   end
 end
