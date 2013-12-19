e6deccb9d58a8da96fa344fd748fe6e8b3e403b3
Volmer Soares <volmerius@gmail.com>
2013-12-18 23:04:40 -0200

    Add host for production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,8 @@ Mundodastrevas::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = { host: 'mundodastrevas.com' }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
