ca69fdd6131aa6c094f20a6f31d88a3450affd8a
Volmer Soares <volmerius@gmail.com>
2014-03-05 12:51:19 -0300

    Fix rank/notification association.

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -2,7 +2,7 @@ class Rank < ActiveRecord::Base
   belongs_to :universe
   has_many :levels, ->(rank){ where(universe: rank.universe) }, foreign_key: 'value', primary_key: 'value'
   has_many :users, through: :levels
-  has_many :notifications, as: :notifiable, dependent: :destroy
+  has_many :notifications, class_name: 'Raddar::Notification', as: :notifiable, dependent: :destroy
 
   validates :universe_id, presence: true
   validates :value,
