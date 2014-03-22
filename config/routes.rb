335b1726b86348add8dcf042d421468973514c6a
Volmer Soares <volmerius@gmail.com>
2014-03-22 15:54:53 -0300

    Redirect old URLs to the new ones.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Mundodastrevas::Application.routes.draw do
   mount Raddar::Engine => '/'
 
   resources :universes, only: [:show]
+
+  # Redirect old URLs to the new ones
+  get '/pubs/:id', to: redirect('/zines/%{id}')
+  get '/pubs/:zine_id/stuffs/:id', to: redirect('/zines/%{zine_id}/posts/%{id}')
+  #get '/forums/:forum_id/topics/:id', to: redirect('/forums/%{forum_id}')
+  #get '/tags/:id', to: redirect {|params, _| "/tags/#{params[:id].gsub('-', ' ')}" }
 end
