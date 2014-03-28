3f8486e16dd70b2a87b2b59e0a189b42ec23069d
Volmer Soares <volmerius@gmail.com>
2014-03-27 22:56:09 -0300

    Use a local var instead of a global one.

--- app/controllers/raddar/home_controller.rb
+++ app/controllers/raddar/home_controller.rb
@@ -3,11 +3,11 @@ module Raddar
     skip_after_action :verify_authorized, only: [:index]
 
     def index
-      @featured_zines = Zines::Zine.with_posts.where(starred: true).order('updated_at DESC').limit(4)
+      featured_zines = Zines::Zine.with_posts.where(starred: true).order('updated_at DESC').limit(4)
 
-      @featured_posts = @featured_zines.map {|zine| zine.posts.order('created_at DESC').first }
+      @featured_posts = featured_zines.map {|zine| zine.posts.order('created_at DESC').first }
 
-      @recent_posts = Zines::Zine.with_posts.where.not(id: @featured_zines.map(&:id)).order('updated_at DESC').limit(5).map {|zine| zine.posts.order('created_at DESC').first }
+      @recent_posts = Zines::Zine.with_posts.where.not(id: featured_zines.map(&:id)).order('updated_at DESC').limit(5).map {|zine| zine.posts.order('created_at DESC').first }
 
       @forum_topics = Forums::Topic.order('created_at DESC').limit(6)
 
