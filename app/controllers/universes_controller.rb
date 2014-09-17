35dc3f88d12aa18332628e39f6e185f307889eea
Volmer Soares <volmerius@gmail.com>
2014-09-17 15:32:31 -0400

    Order zines by last post

--- app/controllers/universes_controller.rb
+++ app/controllers/universes_controller.rb
@@ -5,7 +5,7 @@ class UniversesController < ApplicationController
     authorize(@universe)
 
     @ranks = @universe.ranks.order('value DESC')
-    @zines = @universe.zines.with_posts.where("raddar_zines_zines.image <> ''").order('updated_at DESC').page(params[:page])
+    @zines = @universe.zines.with_posts.where("raddar_zines_zines.image <> ''").order('last_post_at DESC').page(params[:page])
     @forum = @universe.forums.first
     @forum_topics = @forum.topics.order('updated_at DESC').limit(10) if @forum.present?
   end
