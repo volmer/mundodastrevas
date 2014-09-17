e6860cb2a2713bb0b7434eca3e0c36f5885e8402
Volmer Soares <volmerius@gmail.com>
2014-09-17 19:25:18 -0400

    Fix Bootsy inclusion

---
+++ app/controllers/raddar/zines/zines_controller.rb
@@ -0,0 +1,77 @@
+module Raddar
+  module Zines
+    class ZinesController < Zines::ApplicationController
+      before_action :set_zine, only: [:show, :edit, :update, :destroy]
+
+      def index
+        authorize(Zine.new)
+
+        if params[:user_id]
+          @user = User.find_by!(name: params[:user_id])
+
+          @zines = @user.zines.order('COALESCE(last_post_at, created_at) DESC').page(params[:page])
+
+          render 'user_index'
+        else
+          @zines = Zine.with_posts.order('last_post_at DESC').page(params[:page])
+
+          @tags = Tag.trending.limit(8)
+
+          @most_read_posts = Post.order('views DESC').limit(5)
+        end
+      end
+
+      def show
+        @posts = @zine.posts.order('created_at DESC').page(params[:page])
+      end
+
+      def new
+        @zine = Zine.new
+
+        authorize(@zine)
+      end
+
+      def edit
+      end
+
+      def create
+        @zine = Zine.new(zine_params)
+        @zine.user = current_user
+
+        authorize(@zine)
+
+        if @zine.save
+          redirect_to @zine, notice: t('flash.zines.zines.create')
+        else
+          render action: 'new'
+        end
+      end
+
+      def update
+        if @zine.update(zine_params)
+          redirect_to @zine, notice: t('flash.zines.zines.update')
+        else
+          render action: 'edit'
+        end
+      end
+
+      def destroy
+        @zine.destroy
+
+        redirect_to zines_path, notice: t('flash.zines.zines.destroy')
+      end
+
+      private
+
+      def set_zine
+        @zine = Zine.find_by!(slug: params[:id])
+
+        authorize(@zine)
+      end
+
+      def zine_params
+        params.require(:zine).permit(:name, :slug, :description, :image, :universe_id)
+      end
+    end
+  end
+end
