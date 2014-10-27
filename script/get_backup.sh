57f340b829f60a4d149a57c8a4ae961833126393
Volmer Soares <volmerius@gmail.com>
2014-10-27 10:51:20 -0400

    Change host in backup script

--- script/get_backup.sh
+++ script/get_backup.sh
@@ -2,7 +2,7 @@
 
 # Get remote backup
 
-REMOTE_HOST="mundodastrevas.com"
+REMOTE_HOST="107.170.96.245"
 REMOTE_USER="mundodastrevas"
 REMOTE_BACKUP_DIR="/home/mundodastrevas/mundodastrevas_backup"
 LOCAL_BACKUP_DIR="/Users/volmer/backups"
