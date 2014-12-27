19abc3a3ff23335d7b11a2ce577c2469bfcd5597
Volmer Soares <volmerius@gmail.com>
2014-12-26 22:54:39 -0500

    Change home limits

--- app/controllers/raddar/home_controller.rb
+++ app/controllers/raddar/home_controller.rb
@@ -3,17 +3,17 @@ module Raddar
     skip_after_action :verify_authorized, only: [:index]
 
     def index
-      featured_zines = Zine.with_posts.where(starred: true).order('last_post_at DESC').limit(4)
+      featured_zines = Zine.with_posts.where(starred: true).order(last_post_at: :desc).limit(4)
 
-      @featured_posts = featured_zines.map {|zine| zine.posts.order('created_at DESC').first }
+      @featured_posts = featured_zines.map {|zine| zine.posts.order(created_at: :desc).first }
 
-      @recent_posts = Zine.with_posts.where.not(id: featured_zines.map(&:id)).order('last_post_at DESC').limit(5).map {|zine| zine.posts.order('created_at DESC').first }
+      @recent_posts = Zine.with_posts.where.not(id: featured_zines.map(&:id)).order(last_post_at: :desc).limit(4).map {|zine| zine.posts.order('created_at DESC').first }
 
-      @forum_topics = Forums::Topic.order('created_at DESC').limit(6)
+      @forum_topics = Forums::Topic.order(created_at: :desc).limit(4)
 
       @universes = Universe.order(name: :asc)
 
-      @comments = Comment.order('created_at DESC').limit(6)
+      @comments = Comment.order(created_at: :desc).limit(5)
 
       @featured_zine = Zine.find_by(id: Setting[:featured_zine])
     end
