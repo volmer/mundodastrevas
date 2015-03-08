8274283eccd6e531082db91dea5b67f00c250eb3
Volmer Soares <volmer@radicaos.com>
2015-03-07 21:57:01 -0500

    Remove N+1

--- app/controllers/raddar/home_controller.rb
+++ app/controllers/raddar/home_controller.rb
@@ -3,19 +3,21 @@ module Raddar
     skip_after_action :verify_authorized, only: [:index]
 
     def index
-      featured_zines = Zine.with_posts.where(starred: true).order(last_post_at: :desc).limit(4)
+      featured_zines = Zine.with_posts.includes(:posts).where(starred: true).order(last_post_at: :desc).limit(4)
 
-      @featured_posts = featured_zines.map {|zine| zine.posts.order(created_at: :desc).first }
+      @featured_posts = featured_zines.map {|zine| zine.posts.last }
 
-      @recent_posts = Zine.with_posts.where.not(id: featured_zines.map(&:id)).order(last_post_at: :desc).limit(4).map {|zine| zine.posts.order('created_at DESC').first }
+      @recent_posts = Zine.with_posts.includes(:posts).where.not(id: featured_zines.map(&:id)).order(last_post_at: :desc).limit(4).map {|zine| zine.posts.last }
 
-      @forum_topics = Topic.order(created_at: :desc).limit(4)
+      @forum_topics = Topic.includes(:forum).order(created_at: :desc).limit(4)
 
       @universes = Universe.order(name: :asc)
 
-      @comments = Comment.order(created_at: :desc).limit(5)
+      @comments = Comment.includes(:user, post: [:zine]).order(created_at: :desc).limit(5)
 
       @featured_zine = Zine.find_by(id: Setting[:featured_zine])
+
+      @new_members = Raddar::User.where.not(confirmed_at: nil).order(created_at: :desc).limit(6)
     end
   end
 end
