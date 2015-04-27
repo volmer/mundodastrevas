cbbdf709f6df30acba04cebe8de804afbdf3fcbb
Volmer Soares <volmer@radicaos.com>
2015-04-26 23:31:44 -0400

    Reload unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ set :bundle_flags, '--deployment'
 namespace :deploy do
   desc 'Restart application'
   task :restart do
-    invoke 'unicorn:restart'
+    invoke 'unicorn:reload'
 
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
