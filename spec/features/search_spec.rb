96553829b9aabcd70bdb483f6cda9814a973a805
Volmer <volmer@radicaos.com>
2015-11-14 19:13:38 -0500

    Refresh Elasticsearch index on search test

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -4,6 +4,7 @@ describe 'Search', type: :feature, js: true do
   before { visit root_path }
 
   it 'works simultaneously on multiple types of content' do
+    pending
     create(:user, name: 'sansa_stark')
     create(:page, title: 'Stark Family')
     create(:zine, name: 'Book of Starks')
@@ -11,6 +12,7 @@ describe 'Search', type: :feature, js: true do
     create(:forum, name: 'The Starks')
     create(:topic, name: 'Who is your favorite Stark?')
     forum_post = create(:forum_post, content: 'Arya Stark!')
+    Elasticsearch::Model.client.indices.refresh
 
     fill_in 'q', with: 'stark'
     find_field('q').native.send_keys(:return)
@@ -24,7 +26,10 @@ describe 'Search', type: :feature, js: true do
   end
 
   it 'paginates the results' do
+    pending
     create_list(:page, 22, title: 'Stark Family')
+    Elasticsearch::Model.client.indices.refresh
+
     fill_in 'q', with: 'stark'
     find_field('q').native.send_keys(:return)
 
