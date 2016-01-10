2956ea6b133b4d4c6badb4c2de3b2118454d9c20
Volmer <volmer@radicaos.com>
2016-01-09 19:55:53 -0500

    Upgrade to Ruby 2.3

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :linked_dirs, %w{public/uploads public/system log tmp/pids bin tmp/cache tmp
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-set :rbenv_ruby, '2.2.4'
+set :rbenv_ruby, '2.3.0'
 set :rbenv_path, '/home/mundodastrevas/.rbenv'
 
 # Default value for bundle flags is '--deployment --quiet'
