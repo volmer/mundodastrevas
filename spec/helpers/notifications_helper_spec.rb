483e717e8de1559ba47dfb18f5aaf1870c70874d
Volmer <volmer@radicaos.com>
2016-02-29 23:18:22 -0500

    [Boostrap 4] Several fixes

--- spec/helpers/notifications_helper_spec.rb
+++ spec/helpers/notifications_helper_spec.rb
@@ -11,44 +11,4 @@ describe NotificationsHelper, type: :helper do
       expect(helper.unread_notifications_count(user)).to eq(3)
     end
   end
-
-  describe '#last_notifications' do
-    it 'returns the last 4 notifications for the given user' do
-      create_list(:notification, 6, user: user)
-
-      expect(helper.last_notifications(user).count).to eq(4)
-    end
-  end
-
-  describe '#link_to_notification' do
-    let(:notification) { create(:notification) }
-
-    it 'sets the "href" attribute to the notification path' do
-      expect(helper.link_to_notification(notification)).to include(
-        "href=\"#{notification_path(notification)}\"")
-    end
-
-    it 'sets the "id" data attribute to the notification id' do
-      expect(helper.link_to_notification(notification)).to include(
-        "data-id=\"#{notification.id}\"")
-    end
-
-    context 'with an unread notification' do
-      it 'adds the classes "notification" and "unread" to the link' do
-        notification.unread = true
-
-        expect(helper.link_to_notification(notification)).to include(
-          'class="notification unread"')
-      end
-    end
-
-    context 'with a read notification' do
-      it 'adds the classes "notification" and "read" to the link' do
-        notification.unread = false
-
-        expect(helper.link_to_notification(notification)).to include(
-          'class="notification read"')
-      end
-    end
-  end
 end
