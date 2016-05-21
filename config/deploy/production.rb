e81e6ebf4a3801349d6f2ce1398d5026349e7a36
Volmer <git@radicaos.com>
2016-05-21 18:11:25 -0400

    Update prd IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '162.243.93.228', user: 'mundodastrevas', roles: %w(web app db)
+server '107.170.53.49', user: 'mundodastrevas', roles: %w(web app db)
 
 # Custom SSH Options
 # ==================
