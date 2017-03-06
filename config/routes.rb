2a68dc1b0300f7bebde7a6b3bbe425e0706d6f8c
Volmer <git@radicaos.com>
2017-03-06 00:01:22 -0500

    Remove old redirects

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Mundodastrevas::Application.routes.draw do
 
   root 'home#index'
 
-  # Redirect old URLs to the new ones
-  get '/pubs/:id', to: redirect('/zines/%{id}')
-  get '/pubs/:zine_id/stuffs/:id', to: redirect('/zines/%{zine_id}/posts/%{id}')
-
   resources :zines, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
